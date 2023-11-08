5d810f5f2c07b3822ee5a664eb2fa0e7b79cb452
Regina <regina.esluka@gmail.com>
2023-11-07 21:50:51 -0600

    Se realizan test de hito 1 y se realizan funciones correspondientes para hito 2 Close #13, close #14, close #15, close #16, close #17, close #18

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const { isUrlValid, makingGetCall } = require('./lib/app.js')
-const { getAbsolutePath, fileExists, isMarkdown, readFile } = require('./lib/file.js')
-let fs = require('fs'), //fs es una librería que tiene métodos para abrir el archivo y leer su contenido
-    readline = require('readline')
-const { response } = require('express')
+const { getAbsolutePath, fileExists, isMarkdown, getLinksFromFile } = require('./lib/file.js')
 
 const mdLinks = (pathValidate) => {
     const absolutePath = getAbsolutePath(pathValidate)
@@ -10,31 +7,19 @@ const mdLinks = (pathValidate) => {
         .then(((res) => {
             console.log('The file exists: ' + res)
             if (isMarkdown(absolutePath)) {
-                readFile(absolutePath)
-                    .then((links) => {
-                        links.array.forEach((link) => {
-                            makingGetCall(link.href)
+                getLinksFromFile(absolutePath)
+                    .then((items) => {
+                        items.forEach((item) => {
+                            if(isUrlValid(item.href)) {
+                                makingGetCall(item, absolutePath) //aqui pasamos item como todo el object.
                                 .then((response) => {
                                     console.log(response)
                                 })
                                 .catch((e) => console.log(e))
-
+                            }
                         });
 
                     })
-                // const reader = readline.createInterface({ // mi variable reader va a leer el archivo que quiero que lea. 
-                //     input: fs.createReadStream(absolutePath) // COMO CACHAR AQUI SI UN USUARIO METE UN ARCHIVO?????????????
-                // })
-                // reader.on('line', function (line) { //el reader.on da marcha a la lectura linea por linea de mi archivo
-                //     const isUrl = isUrlValid(line) //aqui valido que la linea sea una URL valida 
-                //     if (isUrl) {
-                //         makingGetCall(line).then((response) => { console.log(response) })
-                //             .catch((e) => console.log(e))
-                //         // validUrls.push(line) // la agrega la linea que acaba de leer si es una url VALIDA. 
-                //     }
-                // })
-                // llamas la funcion readfile, le pasas el archivo real. la funcion readfile te regresa los links como array cuando se resuelve
-                // a ese array en el then le tienes que hacer un for each, a cada link lo vas a validar y si es valido mandas llamar makingGetCall
             }
         }))
         .catch((error) => {
@@ -46,6 +31,6 @@ const mdLinks = (pathValidate) => {
         })
 }
 // Ejemplo de uso:
-mdLinks('./example/README.md')
+mdLinks('./README.md')
 
 module.exports = mdLinks
